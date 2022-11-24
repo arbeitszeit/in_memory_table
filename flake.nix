17fcadc71ec30e960c5db90be2b509a908f60ae5
Sebastian Jordan <sebastian.jordan.mail@googlemail.com>
2022-11-24 18:18:59 +0100

    Improve nix flake

--- flake.nix
+++ flake.nix
@@ -1,16 +1,16 @@
 {
-  description = "A very basic flake";
+  description = "Python package for dealing with indexed data in memory";
 
-  outputs = { self, nixpkgs }:
-    let
-      pkgs = import nixpkgs { system = "x86_64-linux"; };
-      python = pkgs.python3.withPackages
-        (p: [ p.black p.isort p.flake8 p.mypy p.pytest p.setuptools ]);
-    in {
-      packages.x86_64-linux.default = python.pkgs.callPackage ./package.nix { };
-      devShells.x86_64-linux.default = pkgs.mkShell {
-        packages =
-          [ python pkgs.nixfmt ];
-      };
-    };
+  inputs = { flake-utils.url = "github:numtide/flake-utils"; };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+        python = pkgs.python3.withPackages
+          (p: [ p.black p.isort p.flake8 p.mypy p.pytest p.setuptools ]);
+      in {
+        packages.default = python.pkgs.callPackage ./package.nix { };
+        devShells.default = pkgs.mkShell { packages = [ python pkgs.nixfmt ]; };
+      });
 }
